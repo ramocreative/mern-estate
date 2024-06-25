b6cfe447b23d4cb696e6811b57d004f662b1b6ab
Raman Seerwan <ramocreative@gmail.com>
2024-06-25 23:22:26 +0300

    complete sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,8 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/signin" element={<SignIn />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
       </Routes>
